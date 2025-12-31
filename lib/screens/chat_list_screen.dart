53d7fe227c60834819c61c5c8cf486d7e5e25fb3
josefinalliende <mariajosefinaalliende@gmail.com>
2025-12-31 11:27:10 -0300

    feat: show chats in chat list

--- lib/screens/chat_list_screen.dart
+++ lib/screens/chat_list_screen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sloth/extensions/build_context.dart';
+import 'package:sloth/hooks/use_chat_list.dart';
 import 'package:sloth/hooks/use_route_refresh.dart';
-import 'package:sloth/hooks/use_welcomes.dart';
 import 'package:sloth/providers/account_pubkey_provider.dart';
-import 'package:sloth/routes.dart';
-import 'package:sloth/src/rust/api/welcomes.dart' show Welcome;
-import 'package:sloth/widgets/chat_list_welcome_tile.dart';
+import 'package:sloth/src/rust/api/groups.dart' show ChatSummary;
+import 'package:sloth/widgets/chat_list_tile.dart';
 import 'package:sloth/widgets/wn_account_bar.dart';
 import 'package:sloth/widgets/wn_slate_container.dart';
 
@@ -17,9 +16,9 @@ class ChatListScreen extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final colors = context.colors;
     final pubkey = ref.watch(accountPubkeyProvider);
-    final welcomesResult = useWelcomes(pubkey);
+    final chatListResult = useChatList(pubkey);
 
-    useRouteRefresh(context, welcomesResult.refresh);
+    useRouteRefresh(context, chatListResult.refresh);
 
     return Scaffold(
       backgroundColor: colors.backgroundPrimary,
@@ -30,8 +29,8 @@ class ChatListScreen extends HookConsumerWidget {
             Expanded(
               child: _buildContent(
                 context,
-                welcomesResult.snapshot,
-                welcomesResult.refresh,
+                chatListResult.snapshot,
+                chatListResult.refresh,
               ),
             ),
           ],
@@ -42,14 +41,14 @@ class ChatListScreen extends HookConsumerWidget {
 
   Widget _buildContent(
     BuildContext context,
-    AsyncSnapshot<List<Welcome>> welcomesSnapshot,
+    AsyncSnapshot<List<ChatSummary>> chatListSnapshot,
     VoidCallback onRefresh,
   ) {
     final colors = context.colors;
 
-    final welcomes = welcomesSnapshot.data ?? [];
+    final chatList = chatListSnapshot.data ?? [];
 
-    if (welcomes.isEmpty) {
+    if (chatList.isEmpty) {
       return RefreshIndicator(
         onRefresh: () async => onRefresh(),
         color: colors.foregroundPrimary,
@@ -97,14 +96,11 @@ class ChatListScreen extends HookConsumerWidget {
       backgroundColor: colors.backgroundTertiary,
       child: ListView.builder(
         physics: const AlwaysScrollableScrollPhysics(),
-        itemCount: welcomes.length,
+        itemCount: chatList.length,
         itemBuilder: (context, index) {
-          final welcome = welcomes[index];
-          return ChatListWelcomeTile(
-            welcome: welcome,
-            onTap: () {
-              Routes.pushToWelcome(context, welcome.id);
-            },
+          final chatSummary = chatList[index];
+          return ChatListTile(
+            chatSummary: chatSummary,
           );
         },
       ),
