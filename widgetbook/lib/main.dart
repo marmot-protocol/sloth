f195eb1de8872dca4d484350de8f25fba14e9912
josefinalliende <mariajosefinaalliende@gmail.com>
2026-01-23 17:33:10 -0300

    chore: setup widgetboook

---
+++ widgetbook/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sloth/theme.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+import 'main.directories.g.dart';
+
+@widgetbook.App()
+void main() {
+  runApp(const WidgetbookApp());
+}
+
+class WidgetbookApp extends StatelessWidget {
+  const WidgetbookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgetbook.material(
+      addons: [
+        ThemeAddon(
+          themes: [
+            WidgetbookTheme(name: 'Light', data: lightTheme),
+            WidgetbookTheme(name: 'Dark', data: darkTheme),
+          ],
+          themeBuilder: (context, theme, child) {
+            return ScreenUtilInit(
+              designSize: const Size(390, 844),
+              minTextAdapt: true,
+              child: Theme(data: theme, child: child),
+            );
+          },
+        ),
+        ViewportAddon(Viewports.all),
+      ],
+      directories: directories,
+    );
+  }
+}
