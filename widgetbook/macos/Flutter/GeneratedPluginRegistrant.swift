e644c08aed8a46289532f9e332b6dab4c91682d0
Pepi <mariajosefinaalliende@gmail.com>
2026-01-29 10:43:20 -0300

    Setup widgetbook with semantic colors story (#82)
    * chore: setup widgetboook
    
    * feat: update colors
    
    * docs: add colors story
    
    * docs: add intro with figma links
    
    * docs: update changelog
    
    * fix: linter issue in wn icon

--- widgetbook/macos/Flutter/GeneratedPluginRegistrant.swift
+++ widgetbook/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import emoji_picker_flutter
 import file_selector_macos
 import flutter_secure_storage_darwin
-import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
@@ -17,7 +16,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
