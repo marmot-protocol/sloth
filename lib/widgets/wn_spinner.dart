3f3405a2d442eda6836f3dd48b96424f7d29224d
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2026-02-03 09:50:11 +0100

    refactor(widgets): convert WnSpinner from StatefulWidget to HookWidget
    Replace manual AnimationController lifecycle management with
    useAnimationController hook to follow repo's hooks-based pattern.

--- lib/widgets/wn_spinner.dart
+++ lib/widgets/wn_spinner.dart
@@ -1,48 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart'
+    show HookWidget, useAnimationController, useEffect;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sloth/theme.dart';
 
 enum WnSpinnerType { primary, secondary, destructive }
 
-class WnSpinner extends StatefulWidget {
+class WnSpinner extends HookWidget {
   const WnSpinner({super.key, this.type = WnSpinnerType.primary});
 
   final WnSpinnerType type;
 
   @override
-  State<WnSpinner> createState() => _WnSpinnerState();
-}
-
-class _WnSpinnerState extends State<WnSpinner> with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 900),
-    )..repeat();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final colors = context.colors;
-    final indicatorColor = _getColor(colors);
+
+    final controller = useAnimationController(
+      duration: const Duration(milliseconds: 900),
+    );
+
+    useEffect(() {
+      controller.repeat();
+      return null;
+    }, const []);
+
+    final indicatorColor = _getColor(colors, type);
 
     return SizedBox.square(
       dimension: 16.w,
       child: AnimatedBuilder(
-        animation: _controller,
+        animation: controller,
         builder: (context, child) {
           return Transform.rotate(
-            angle: _controller.value * 2 * 3.14159265359,
+            angle: controller.value * 2 * 3.14159265359,
             child: child,
           );
         },
@@ -57,8 +47,8 @@ class _WnSpinnerState extends State<WnSpinner> with SingleTickerProviderStateMix
     );
   }
 
-  Color _getColor(SemanticColors colors) {
-    return switch (widget.type) {
+  Color _getColor(SemanticColors colors, WnSpinnerType type) {
+    return switch (type) {
       WnSpinnerType.primary => colors.fillContentPrimary,
       WnSpinnerType.secondary => colors.fillContentSecondary,
       WnSpinnerType.destructive => colors.fillContentDestructive,
