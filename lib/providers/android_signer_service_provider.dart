1139b5a9521c1e12ff7ec4350a11c25d39438191
josefinalliende <mariajosefinaalliende@gmail.com>
2026-02-05 03:12:13 -0300

    refactor: remove unnecessary android service provider

--- lib/providers/android_signer_service_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sloth/services/android_signer_service.dart';
-
-final androidSignerServiceProvider = Provider<AndroidSignerService>((ref) {
-  return AndroidSignerService(platformIsAndroid: Platform.isAndroid);
-});
