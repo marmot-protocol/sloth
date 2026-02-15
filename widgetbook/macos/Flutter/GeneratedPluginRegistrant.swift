b32f620e7eb36c8881d425d725357a82ad336d09
josefinalliende <mariajosefinaalliende@gmail.com>
2026-02-15 02:07:54 -0300

    chore: add flutter local notifications and flutter foreground task deps

--- widgetbook/macos/Flutter/GeneratedPluginRegistrant.swift
+++ widgetbook/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import emoji_picker_flutter
 import file_selector_macos
+import flutter_local_notifications
 import flutter_secure_storage_darwin
 import mobile_scanner
 import shared_preferences_foundation
@@ -16,6 +17,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
